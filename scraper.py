e02cf64be481e0847936e26db77a8c6dd75780b3
ta-dd <141835908+ta-dd@users.noreply.github.com>
2024-03-25 11:01:03 +0100

    cleanup

--- scraper.py
+++ scraper.py
@@ -5,30 +5,22 @@ sys.path.append("cmzf-redataprocessing/redataprocessing/src")
 
 import redataprocessing as rdp
 
-DB_NAME = "test9.sqlite"
+DB_NAME = "2024-03-25.sqlite"
 
-# run main function + return the name of the table suffix
-db_table_name = rdp.get_re_offers(
-    path_to_sqlite=DB_NAME,
-    category_main="landplots",
-    category_type="sale",  # add "auction" here as well? and make sure to label auctions that are under "sales" (v listingu je nejnižší možná nabídka)
-    locality_region=["Liberecký kraj"],
-)
 
-con = sqlite3.connect(DB_NAME)
+def merge_tables(db_table_name):
+    con = sqlite3.connect(DB_NAME)
 
-merged_table_name = "MERGED_" + db_table_name
-query = f"""
-    CREATE TABLE IF NOT EXISTS {merged_table_name} AS
-    SELECT o.*, d.*
-    FROM {"OFFERS_" + db_table_name} o
-    LEFT JOIN {"DESCRIPTION_" + db_table_name} d ON o.hash_id = d.hash_id
-"""
-con.execute(query)
-con.commit()
-con.close()
-
-sys.exit()
+    merged_table_name = "MERGED_" + db_table_name
+    query = f"""
+        CREATE TABLE IF NOT EXISTS {merged_table_name} AS
+        SELECT o.*, d.*
+        FROM {"OFFERS_" + db_table_name} o
+        LEFT JOIN {"DESCRIPTION_" + db_table_name} d ON o.hash_id = d.hash_id
+    """
+    con.execute(query)
+    con.commit()
+    con.close()
 
 
 kraje = [
@@ -51,9 +43,10 @@ kraje = [
 for kraj in kraje:
     print(f"-----------------------------------------")
     print(f"--- Downloading {kraj} ---")
-    rdp.get_re_offers(
-        path_to_sqlite="test2.sqlite",
+    db_table_name = rdp.get_re_offers(
+        path_to_sqlite=DB_NAME,
         category_main="landplots",
         category_type="sale",
         locality_region=[kraj],
     )
+merge_tables(db_table_name=db_table_name)
